e2b49f89ead80d44fc1a93979e91881bf91854c0
s.fedorovich <s.fedorovich@CZC71776GT>
2021-10-28 15:22:30 +0300

    Задача 1 и 2

--- Задача 2/Program.cs
+++ Задача 2/Program.cs
@@ -10,6 +10,24 @@ namespace Задача_2
     {
         static void Main(string[] args)
         {
+            {//метод для вычисления объёма и площади поверхности куба по длине его ребра
+                {
+                    Console.Write("Ввести длину ребра куба a:  ");
+                    double a = Convert.ToDouble(Console.ReadLine());
+                    double v = 0;
+                    double s = 0;
+                    getParam(a, out v, out s);
+                    Console.WriteLine("Объём куба с ребром a равен:   {0}", v);
+                    Console.WriteLine("Площадь поверхности куба с ребром a равна:   {0}", s);
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        private static void getParam(double a, out double v, out double s)
+        {
+            v = a * a * a;
+            s = 6 * a * a;
         }
     }
 }
